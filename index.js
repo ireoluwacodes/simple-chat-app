498d3b4fd1b26202ef902344afa1a1068f0313a0
Ireoluwatobi <111185454+Ireoluwatobi@users.noreply.github.com>
2024-02-12 15:40:45 +0100

    finished simple chat

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
 const { PORT } = require("./config/constants");
 const app = express();
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 const server = app.listen(PORT, () =>
   console.log(`server is listening on port ${PORT}`)
 );
 
-const io = require("socket.io")(server)
+const io = require("socket.io")(server);
 
+let socketsConnected = new Set();
 
-const onConnected = (socket)=>{
-    console.log(socket.id)
-}
+const onConnected = (socket) => {
+  console.log(socket.id);
+  socketsConnected.add(socket.id);
 
-io.on("connection", onConnected)
+  io.emit("connected-clients", socketsConnected.size);
+
+  socket.on("disconnect", () => {
+    console.log(`Socket with id ${socket.id} disconnected`);
+    socketsConnected.delete(socket.id);
+
+    io.emit("connected-clients", socketsConnected.size);
+  });
+
+  socket.on("message", (data) => {
+    socket.broadcast.emit("chats", data);
+  });
+
+  socket.on("feedback", (data)=>{
+    socket.broadcast.emit("feedback", data)
+  })
+};
+
+io.on("connection", onConnected);
--- public/main.js
+++ public/main.js
@@ -1,90 +1,92 @@
-const socket = io()
+const socket = io();
 
-const clientsTotal = document.getElementById('client-total')
+const clientsTotal = document.getElementById("client-total");
 
-const messageContainer = document.getElementById('message-container')
-const nameInput = document.getElementById('name-input')
-const messageForm = document.getElementById('message-form')
-const messageInput = document.getElementById('message-input')
+const messageContainer = document.getElementById("message-container");
+const nameInput = document.getElementById("name-input");
+const messageForm = document.getElementById("message-form");
+const messageInput = document.getElementById("message-input");
 
-const messageTone = new Audio('/message-tone.mp3')
+const messageTone = new Audio("/message-tone.mp3");
 
-messageForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-  sendMessage()
-})
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendMessage();
+});
 
-socket.on('clients-total', (data) => {
-  clientsTotal.innerText = `Total Clients: ${data}`
-})
+socket.on("connected-clients", (data) => {
+  clientsTotal.innerText = `Total Clients: ${data}`;
+});
 
 function sendMessage() {
-  if (messageInput.value === '') return
+  let string = messageInput.value;
+  let validity = string.search(/\S/);
+  if (messageInput.value === "" || validity == -1) return;
   // console.log(messageInput.value)
   const data = {
     name: nameInput.value,
     message: messageInput.value,
     dateTime: new Date(),
-  }
-  socket.emit('message', data)
-  addMessageToUI(true, data)
-  messageInput.value = ''
+  };
+  socket.emit("message", data);
+  addMessageToUI(true, data);
+  messageInput.value = "";
 }
 
-socket.on('chat-message', (data) => {
+socket.on("chats", (data) => {
   // console.log(data)
-  messageTone.play()
-  addMessageToUI(false, data)
-})
+  messageTone.play();
+  addMessageToUI(false, data);
+});
 
 function addMessageToUI(isOwnMessage, data) {
-  clearFeedback()
+  clearFeedback();
   const element = `
-      <li class="${isOwnMessage ? 'message-right' : 'message-left'}">
+      <li class="${isOwnMessage ? "message-right" : "message-left"}">
           <p class="message">
             ${data.message}
             <span>${data.name} ● ${moment(data.dateTime).fromNow()}</span>
           </p>
         </li>
-        `
+        `;
 
-  messageContainer.innerHTML += element
-  scrollToBottom()
+  messageContainer.innerHTML += element;
+  scrollToBottom();
 }
 
 function scrollToBottom() {
-  messageContainer.scrollTo(0, messageContainer.scrollHeight)
+  messageContainer.scrollTo(0, messageContainer.scrollHeight);
 }
 
-messageInput.addEventListener('focus', (e) => {
-  socket.emit('feedback', {
+messageInput.addEventListener("focus", (e) => {
+  socket.emit("feedback", {
     feedback: `✍️ ${nameInput.value} is typing a message`,
-  })
-})
+  });
+});
 
-messageInput.addEventListener('keypress', (e) => {
-  socket.emit('feedback', {
+messageInput.addEventListener("keypress", (e) => {
+  socket.emit("feedback", {
     feedback: `✍️ ${nameInput.value} is typing a message`,
-  })
-})
-messageInput.addEventListener('blur', (e) => {
-  socket.emit('feedback', {
-    feedback: '',
-  })
-})
+  });
+});
+messageInput.addEventListener("blur", (e) => {
+  socket.emit("feedback", {
+    feedback: "",
+  });
+});
 
-socket.on('feedback', (data) => {
-  clearFeedback()
+socket.on("feedback", (data) => {
+  clearFeedback();
   const element = `
         <li class="message-feedback">
           <p class="feedback" id="feedback">${data.feedback}</p>
         </li>
-  `
-  messageContainer.innerHTML += element
-})
+  `;
+  messageContainer.innerHTML += element;
+});
 
 function clearFeedback() {
-  document.querySelectorAll('li.message-feedback').forEach((element) => {
-    element.parentNode.removeChild(element)
-  })
+  document.querySelectorAll("li.message-feedback").forEach((element) => {
+    element.parentNode.removeChild(element);
+  });
 }
